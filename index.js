9abcae53bfa698d7cf3d068403ac59951eb911b9
gaeun <geun5744@gmail.com>
2022-11-11 04:12:00 +0900

    auth.js 추가

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
+const authRoute = require("./routes/auth");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -22,6 +22,7 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+app.use("/auth", authRoute);
 app.listen(PORT, async (req, res) => {
   console.log("Server is listening on localhost:5000");
 });
