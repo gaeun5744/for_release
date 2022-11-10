5b9164dec59c9e08883049f1a178c391ad2da6e6
gaeun <geun5744@gmail.com>
2022-11-11 02:51:20 +0900

    auth.js 삭제

--- routes/auth.js
+++ routes/auth.js
@@ -3,19 +3,6 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 // Register
-router.post("/register", async (req, res) => {
-  try {
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post("/register", async (req, res) => {});
 
 module.exports = router;
