4e10efe7b23506e9314b8c7b2c5160d7448a9e0d
gaeun <geun5744@gmail.com>
2022-11-11 00:40:10 +0900

    엔진 버전, Procfile 추가

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const PORT = process.env.PORT || 5000;
 // 이거 건드리지 말기!!!
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
 }
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
-
-app.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
 
 app.listen(PORT, async (req, res) => {
   console.log("Server is listening on localhost:5000");
