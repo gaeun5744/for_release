737bbef325ffaa9f5ccb3895afa13e5aa8ca6509
gaeun <geun5744@gmail.com>
2022-11-10 12:58:50 +0900

    몽고DB 연결

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// 이거 건드리지 말기!!!
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
 }
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, async (req, res) => {
   console.log("Server is listening on localhost:5000");
