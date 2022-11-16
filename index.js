b94ae2b1355efc896f07938c876ba2eca4a351ac
gaeun <geun5744@gmail.com>
2022-11-17 00:00:57 +0900

    백엔드 api 추가

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
 const app = express();
+const port = 5000;
+
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
-dotenv.config();
 
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
@@ -26,4 +30,27 @@ mongoose
 app.use("/auth", authRoute);
 app.listen(PORT, async (req, res) => {
   console.log("Server is listening on localhost:5000");
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen(port, () => {
+  console.log("Backend is running");
 });
