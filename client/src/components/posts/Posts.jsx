5d46fb5310eddd2dfdf2b62dd73cd885569634b9
gaeun <geun5744@gmail.com>
2022-11-17 00:16:04 +0900

    axios로 DB와 프론트 연결

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -5,14 +5,9 @@ import React from "react";
 export default function Posts() {
   return (
     <div className="posts">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {Posts.map((p) => (
+        <Post />
+      ))}
     </div>
   );
 }
