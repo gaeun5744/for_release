9fced56fb1a46d5e1b63995aa6789831b9206ddd
gaeun <geun5744@gmail.com>
2022-11-25 00:15:04 +0900

    경로 수정 전으로 되돌리기

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -32,7 +32,6 @@ export default function Write() {
       const res = await axios.post("/posts", newPost);
       window.location.replace("/post/" + res.data._id);
     } catch (err) {}
-    axios.post("/posts", newPost);
   };
   return (
     <div className="write">
