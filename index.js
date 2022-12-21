b55aa3a651e46dafd879934b514510c2ee13a64f
gaeun <geun5744@gmail.com>
2022-12-21 21:50:56 +0900

    index.js dotenv.confit(); 추가

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
